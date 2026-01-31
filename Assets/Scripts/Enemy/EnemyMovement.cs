3deaa24fb80f7453f52a5c8f07bcb2cb90f2cec5
Rass-i <rasmusevr@gmail.com>
2026-01-31 13:22:42 +0100

    :)

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -18,22 +20,34 @@ public class EnemyMovement : MonoBehaviour
     agent = GetComponent<NavMeshAgent> ();
     timer = wanderTimer;
     timer = waitTimer;
+    StartCoroutine(wander());
+  }
+
+  IEnumerator wander()
+  {
+    yield return new WaitForSeconds (2);
+    Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+    agent.SetDestination(newPos);
+    yield return new WaitForSeconds (5);
+    agent.ResetPath ();
+    StartCoroutine (wander());
   }
   
   // Update is called once per frame
   void Update ()
   {
-    timer += Time.deltaTime;
+   /* timer += Time.deltaTime;
 
     if (timer >= wanderTimer)
     {
       Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
       agent.SetDestination(newPos);
       timer = 0;
-    }
+      
+    }*/
   }
 
-  public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+  public Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
   {
     Vector3 randDirection = Random.insideUnitSphere * dist;
 
@@ -42,6 +56,8 @@ public class EnemyMovement : MonoBehaviour
     NavMeshHit navHit;
 
     NavMesh.SamplePosition (randDirection, out navHit, dist, layermask);
+
+    timer = wanderTimer + Time.time;
 
     return navHit.position;
   }
